22a86930275dbb725359abb33ab7fa91f50c8387
jslucas <jspencerlucas@gmail.com>
2021-04-24 12:24:11 -0400

    refactor roman numerals to use conversion tuple list instead of function clauses

--- elixir/roman-numerals/lib/roman_numerals.ex
+++ elixir/roman-numerals/lib/roman_numerals.ex
@@ -1,22 +1,30 @@
 defmodule RomanNumerals do
+  @roman_to_arabic [
+    {"M", 1_000},
+    {"CM", 900},
+    {"D", 500},
+    {"CD", 400},
+    {"C", 100},
+    {"XC", 90},
+    {"L", 50},
+    {"XL", 40},
+    {"X", 10},
+    {"IX", 9},
+    {"V", 5},
+    {"IV", 4},
+    {"I", 1}
+  ]
+
   @doc """
   Convert the number to a roman number.
   """
   @spec numeral(pos_integer) :: String.t()
-  def numeral(number), do: numeralize(number)
+  def numeral(0), do: ""
 
-  defp numeralize(0), do: ""
-  defp numeralize(number) when number >= 1000, do: "M" <> numeralize(number - 1000)
-  defp numeralize(number) when number >= 900, do: "CM" <> numeralize(number - 900)
-  defp numeralize(number) when number >= 500, do: "D" <> numeralize(number - 500)
-  defp numeralize(number) when number >= 400, do: "CD" <> numeralize(number - 400)
-  defp numeralize(number) when number >= 100, do: "C" <> numeralize(number - 100)
-  defp numeralize(number) when number >= 90, do: "XC" <> numeralize(number - 90)
-  defp numeralize(number) when number >= 50, do: "L" <> numeralize(number - 50)
-  defp numeralize(number) when number >= 40, do: "XL" <> numeralize(number - 40)
-  defp numeralize(number) when number >= 10, do: "X" <> numeralize(number - 10)
-  defp numeralize(number) when number == 9, do: "IX" <> numeralize(number - 9)
-  defp numeralize(number) when number >= 5, do: "V" <> numeralize(number - 5)
-  defp numeralize(number) when number == 4, do: "IV" <> numeralize(number - 4) 
-  defp numeralize(number) when number < 4, do: "I" <> numeralize(number - 1)
+  def numeral(number) do
+    {roman, arabic} = Enum.find(@roman_to_arabic, fn {_roman, arabic} -> number >= arabic end)
+
+    roman <> numeral(number - arabic)  
+  end
+
 end
